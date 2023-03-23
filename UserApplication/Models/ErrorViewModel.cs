f68842c362de0fac05ff5f271673a156712703b9
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2023-03-23 20:34:34 +0530

    pr-init

---
+++ UserApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace UserApplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
